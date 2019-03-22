916a89c349b1103d75b62da804252ca1fe8df7f8
sandy.Zhao <Sandy.Zhao@DESKTOP-5QHA8U7>
2019-03-22 19:28:55 +0800

    分页组件

---
+++ aspnet-core/src/AbpDemoOne.Web.Mvc/Views/Shared/Components/Pagination/PaginationViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Abp.Configuration;
+using AbpDemoOne.Configuration;
+using AbpDemoOne.Configuration.Ui;
+
+namespace AbpDemoOne.Web.Views.Shared.Components.Pagination
+{
+    public class PaginationViewComponent : AbpDemoOneViewComponent
+    {
+        private readonly ISettingManager _settingManager;
+
+        public PaginationViewComponent(ISettingManager settingManager)
+        {
+            _settingManager = settingManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int pageCont)
+        {
+           
+            var viewModel = new PaginationViewModel
+            {
+                PageCont = 10
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ aspnet-core/src/AbpDemoOne.Web.Mvc/Views/Shared/Components/Pagination/PaginationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AbpDemoOne.Configuration.Ui;
+
+namespace AbpDemoOne.Web.Views.Shared.Components.Pagination
+{
+    public class PaginationViewModel
+    {
+        public int PageCont { get; set; }
+    }
+}
