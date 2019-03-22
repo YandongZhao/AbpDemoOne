ae186e6c692da8e49c38ccca5a6568161410d376
sandy.Zhao <Sandy.Zhao@DESKTOP-5QHA8U7>
2019-03-22 13:44:52 +0800

    修改注册

--- aspnet-core/src/AbpDemoOne.Web.Mvc/wwwroot/view-resources/Views/Account/Register.js
+++ aspnet-core/src/AbpDemoOne.Web.Mvc/wwwroot/view-resources/Views/Account/Register.js
@@ -25,17 +25,6 @@
                 }
             },
 
-            highlight: function (input) {
-                $(input).parents('.form-line').addClass('error');
-            },
-
-            unhighlight: function (input) {
-                $(input).parents('.form-line').removeClass('error');
-            },
-
-            errorPlacement: function (error, element) {
-                $(element).parents('.form-group').append(error);
-            }
         });
     });
 
